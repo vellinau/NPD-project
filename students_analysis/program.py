d9ed499deae44fbffd9a0cf96e94e96911ce6fe4
Paweł Brachaczek <pawel.w.brachaczek@gmail.com>
2021-10-26 21:09:55 +0200

    last fixes

--- students_analysis/program.py
+++ students_analysis/program.py
@@ -7,17 +7,17 @@ from src import per_teacher_analysis, per_age_analysis
 
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('schools_path', type=str, help='path to .xlsx file with schools data',
+parser.add_argument('schools_path', nargs='?', type=str, help='path to .xlsx file with schools data',
                     default='data\\Wykaz_szkół_i_placówek_wg_stanu_na_30.IX._2018_w.5.xlsx')
-parser.add_argument('population_path', type=str, help='path to directory with population data',
+parser.add_argument('population_path', nargs='?', type=str, help='path to directory with population data',
                     default='data\\Ludność. Stan i struktura_30.06.2020')
-parser.add_argument('outpath_gmina', type=str, help='where to save results of per teacher '
+parser.add_argument('outpath_gmina', nargs='?', type=str, help='where to save results of per teacher '
                                                     'analysis, for each gmina',
                     default='results\\gmina_per_teacher.xlsx')
-parser.add_argument('outpath_gminatype', type=str, help='where to save results of per teacher '
+parser.add_argument('outpath_gminatype', nargs='?', type=str, help='where to save results of per teacher '
                                                         'analysis, in total',
                     default='results\\gminatype_per_teacher.xlsx')
-parser.add_argument('outpath_per_age', type=str, help='where to save results of per age '
+parser.add_argument('outpath_per_age', nargs='?', type=str, help='where to save results of per age '
                                                       'analysis, in total',
                     default='results\\gminatype_per_age.xlsx')
 args = parser.parse_args()
